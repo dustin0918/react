fad852002f7abd46f17127deec1cd404b5bc051c
dustinxie <dustinxie@hotmail.com>
2019-07-27 16:16:38 -0700

    setting start

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 // Import the React and ReactDom libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+// create a react component
+const App = ()=>{
+    return <div>Hi there!</div>;
+};
+
+// Take the react component and show it on the screen
+ReactDOM.render(
+    <App/>,
+    document.querySelector('#root')
+)
