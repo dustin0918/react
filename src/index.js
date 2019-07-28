6677c2950f7b747a723d0b0f527ecfb7ca768979
dustinxie <dustinxie@hotmail.com>
2019-07-27 17:01:59 -0700

    building content with jsx

--- src/index.js
+++ src/index.js
@@ -6,7 +6,18 @@ import ReactDOM from 'react-dom';
 
 // create a react component
 const App = ()=>{
-    return <div>Hi there!</div>;
+    const buttonText ={ text:'click me'};
+    const labelText ='enter name:';
+    return (
+    <div>
+    <label className="label" htmlFor="name">
+    {labelText}
+    </label>
+    <input id="name" type="text"/>
+        <button style={{backgroundColor:"blue", color:"white"}}>
+        {buttonText.text}</button>
+    </div>
+    );
 };
 
 // Take the react component and show it on the screen
